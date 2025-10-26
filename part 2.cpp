42b06151d759e5d2e2cc88ca4c9276e7e925d0e3
Mitzi Pua <55215483+fulinelite@users.noreply.github.com>
2025-10-25 19:43:22 -0700

    Add files via upload

---
+++ part 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <array>
+#include <random>
+#include <iomanip>
+
+
+using namespace std; 
+
+main int(){
+    double rollDice;
+    int diceRolls 
+    diceRoll= 1: 36000;
+    dice = 2; 
+    int sides;
+    sides = 6;
+    int sides [13]= {0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; 
+
+     while ( diceRoll > 36000, )
+    if ()
+    do
+     {
+        rand rollDice (1 : 36000)
+    } while dice ()
+    for ( sides = 6, 1 : 6 )
+//Dice roll possibly??? I don't remember what this code does lol// 
+}
