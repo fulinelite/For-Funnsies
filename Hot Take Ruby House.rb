52b99151e4f392cfe2dd2dbc7ab23d85ce4230d7
Mitzi Pua <55215483+fulinelite@users.noreply.github.com>
2025-11-02 21:57:39 -0800

    Hot Take Ruby House
    Just a reminder for myself.

---
+++ Hot Take Ruby House.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby -w 
+puts "Hello, World!";#FFC0CB#FFC0CB
+puts "  __________    /\ ";
+puts "  |        |   /Uh\ ";
+puts "  |        |  /hey!\ " ;
+puts "  |        | / You! \ ";
+puts "  |        |/ Don't  \ ";
+puts "  |        /be a sad  \ ";
+puts "  |       /code monkey \ ";
+puts "  |      / if you use   \ " ;
+puts "  |     / AI to generate \ ";
+puts "  |    /  all your code   \ ";
+puts "  |   /  without the       \ ";
+puts "  |  /understanding of the  \ ";
+puts "  | /actual language you're  \ ";
+puts "  |/trying to automate so     \ ";
+puts "  /don't forget to actually    \ ";
+puts " /code!!! So don't forget the   \ ";
+puts "/ fundamentals along the way!!!  \ ";
+puts "-----------------------------------" ;
+puts "|                                 |";
+puts "|                                 |";
+puts "|  ________            _________  |";
+puts "| |        |          |         | |";
+puts "| |        |          |         | |";
+puts "| |________| ________ |_________| |";
+puts "|           |        |            |";
+puts "|           |        |            |";
+puts "|           |      {}|            |";
+puts "|___________|________|____________|";
+end
+
+#Basic hello world output
+# puts is a command that is able to breaks and new lines unlike the command print
+#Command print has outputs that is continuous 
